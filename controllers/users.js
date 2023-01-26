f0205f151ccb2807276df9f0ccf58cde5ef1c64f
jackhr <jc2o@mac.com>
2023-01-26 12:51:22 -0500

    Clear useless code and uninstall node-localstorage

--- controllers/users.js
+++ controllers/users.js
@@ -1,14 +1,6 @@
 const User = require('../models/user');
 const jwt = require('jsonwebtoken');
 
-if (typeof localStorage === "undefined" || localStorage === null) {
-  var LocalStorage = require('node-localstorage').LocalStorage;
-  localStorage = new LocalStorage('./scratch');
-}
-
-localStorage.setItem('myFirstKey', 'myFirstValue');
-console.log(localStorage.getItem('myFirstKey'));
-
 module.exports = {
   getAll,
   create
