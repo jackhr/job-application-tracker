5f858623e51d7cc2b7bff6393205ebdcd24ea218
jackhr <jc2o@mac.com>
2023-01-26 08:51:49 -0500

    Update user model

--- models/user.js
+++ models/user.js
@@ -1,12 +1,49 @@
+const bcrypt = require('bcrypt');
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const SALT_ROUNDS = 6;
+
 const userSchema = new Schema({
-  name: String,
-  email: String,
-  avatar: String
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    required: true
+  },
+  password: {
+    type: String,
+    trim: true,
+    minLength: 5,
+    required: true
+  }
 }, {
-  timestamps: true
+  timestamps: true,
+  toJSON: {
+    transform: function(doc, ret) {
+      delete ret.password;
+      return ret;
+    }
+  }
+});
+
+userSchema.pre('save', function(next) {
+  // Save the reference to the user doc
+  const user = this;
+  if (!user.isModified('password')) return next();
+  // password has changed - salt and hash it
+  bcrypt.hash(user.password, SALT_ROUNDS, function(err, hash) {
+    if (err) return next(err);
+    // Update the password property with the hash
+    user.password = hash;
+    return next();
+  });
 });
 
 module.exports = mongoose.model('User', userSchema);
