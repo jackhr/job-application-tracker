77ace1f64cede4b3ecddd2af6d5e122c9c1ee39e
jackhr <jc2o@mac.com>
2023-01-31 12:56:03 -0500

    Add applicationCount to user model and make all crud functions asynchronous

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,5 +1,6 @@
 const Job = require('../models/job');
 const Contact = require('../models/contact');
+const User = require('../models/user');
 
 module.exports = {
   create,
@@ -8,7 +9,6 @@ module.exports = {
 };
 
 async function update(req, res) {
-
   const job = await Job.findById(req.params.id);
 
   for (jobField in req.body) {
@@ -21,28 +21,42 @@ async function update(req, res) {
 }
 
 async function create(req, res) {
-
-  const contact = await Contact.create({});
-  const job = await Job.create({
-    contact: contact._id,
-    user: req.user._id
-  });
-
-  job.contact = contact;
-
-  res.json(job);
+  try {
+    const user = await User.findById(req.user._id);
+    const contact = await Contact.create({});
+    const job = await Job.create({
+      companyName: `My New Company ${++user.applicationCount}`,
+      contact: contact._id,
+      user: req.user._id
+    });
+    await user.save();
+    job.contact = contact;
+    res.json(job);
+  } catch(error) {
+    res.json({
+      error: error.message
+    });
+  }
 }
 
-function deleteOne(req, res) {
-  Job.findByIdAndDelete(req.params.id, function(err, job) {
-    if (err) console.log(err);
+async function deleteOne(req, res) {
+  try {
+    const job = await Job.findByIdAndDelete(req.params.id);
+    const user = await User.findById(req.user._id);
+    user.applicationCount--;
+    await user.save();
+    await Contact.findByIdAndDelete(job.contact);
     if (req.xhr) {
-      if (err) return res.json(err);
       return res.json({
         status: 200,
         message: `Job: ${job.title}, successfully deleted`
       });
     }
     res.redirect(`/users/${job.user}`);
-  })
+  } catch(err) {
+    if (err) {
+      console.log(err);
+      if (req.xhr) return res.json(err);
+    }
+  }
 }
