cfc390421eb4026deac328635f8824ba3e9dc742
jackhr <jc2o@mac.com>
2023-02-28 15:45:28 -0500

    Correct ensureLoggedIn if statement

--- config/ensureLoggedIn.js
+++ config/ensureLoggedIn.js
@@ -1,5 +1,5 @@
 module.exports = function(req, res, next) {
-  if (req.session.loggedOut) {
+  if (!req.session.loggedIn) {
     if (req.xhr) return res.json({ loggedOut: true });
     return res.redirect('/');
   }
